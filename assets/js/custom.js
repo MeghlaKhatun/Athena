a40fed4b64d3a8d916ce4a9dfaf7848de3d6c93c
meghla khatun <meghlaparvin45@gmail.com>
2024-08-29 23:17:57 +0600

    added a navbar responsive all the device

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,5 @@
 const menuBar = document.querySelector('.fa-bars');
 const menu = document.querySelector('.menu-bar');
-menuBar.addEventListener('click', ()=>{
+menuBar.addEventListener('click', () => {
 	menu.classList.toggle("active")
 })
